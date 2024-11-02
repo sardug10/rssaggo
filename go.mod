6c2f203da7f03e6b06ca5b163ea0fbd3ca9eb896
Sarthak Duggal <sarthak.duggal@tifin.com>
2024-11-02 20:35:11 +0530

    added env config

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sardug10/rssaggo
 
 go 1.21.4
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("hello world")
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	portString:= os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT not defined")
+	}
+	fmt.Println("Server running on port", portString)
 }
